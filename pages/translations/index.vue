d408f8e160b35cf0b3aade742a2595d45c13b629
ogabeksanoyev06 <ogabeksanoyev06@gmail.com>
2024-09-14 13:14:32 +0500

    .

--- pages/translations/index.vue
+++ pages/translations/index.vue
@@ -98,7 +98,26 @@
             </div>
          </div>
          <div class="flex justify-end">
-            <Button type="submit">Saqlash</Button>
+            <Button type="submit">
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true"
+                  role="img"
+                  font-size="48"
+                  class="animate-spin iconify iconify--ph w-5 h-5"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 256 256"
+                  v-if="loading"
+               >
+                  <path
+                     fill="#fff"
+                     d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m39.11 25.19C170.24 83.71 155 99.44 135 113.61c-2.25-24.48-8.44-49.8-38.37-67.82a87.89 87.89 0 0 1 70.5 3.4ZM40.18 133.54c28.34-20 49.57-14.68 71.87-4.39c-20.05 14.19-38.86 32.21-39.53 67.11a87.92 87.92 0 0 1-32.34-62.72m136.5 67.73c-31.45-14.55-37.47-35.58-39.71-60c12.72 5.86 26.31 10.75 41.3 10.75c11.33 0 23.46-2.8 36.63-10.08a88.2 88.2 0 0 1-38.22 59.33"
+                  ></path>
+               </svg>
+               Saqlash
+            </Button>
          </div>
       </VForm>
    </div>
@@ -134,13 +153,12 @@ const addTranslationGroup = () => {
 };
 
 const prepareTranslationData = (lang) => {
-   const data = {};
-   translationGroups.value.forEach((group) => {
+   return translationGroups.value.reduce((data, group) => {
       if (group.key && group[lang]) {
          data[group.key] = group[lang];
       }
-   });
-   return data;
+      return data;
+   }, {});
 };
 
 const handleSubmitForm = async () => {
@@ -151,7 +169,7 @@ const handleSubmitForm = async () => {
       const ruData = prepareTranslationData('ru');
       const enData = prepareTranslationData('en');
 
-      await Promise.all([
+      const [uzResponse, uzcResponse, kzResponse, ruResponse, enResponse] = await Promise.all([
          AddOrUpdateTranslations('uz', uzData),
          AddOrUpdateTranslations('uzc', uzcData),
          AddOrUpdateTranslations('kz', kzData),
@@ -159,8 +177,15 @@ const handleSubmitForm = async () => {
          AddOrUpdateTranslations('en', enData)
       ]);
 
+      console.log('UZ translation response:', uzResponse);
+      console.log('UZC translation response:', uzcResponse);
+      console.log('KZ translation response:', kzResponse);
+      console.log('RU translation response:', ruResponse);
+      console.log('EN translation response:', enResponse);
+
    } catch (error) {
       console.error('Xatolik yuz berdi:', error);
    }
 };
+
 </script>
